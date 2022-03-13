1955f1507d865a0b1bfc47a83cc487d4aeff6d8f
Mechaor <98327407+Mechaor@users.noreply.github.com>
2022-03-14 01:06:37 +0300

    Update county.c

--- county.c
+++ county.c
@@ -8,24 +8,54 @@ C99 Compiler
 #include <stdio.h>
 #include <stdlib.h>
 
-void menu()
-{
-    int action;
-printf("select an action below");
-printf("1. add new patron\n");
-printf("2. view all patrons\n");
-printf("3. add new Resources\n");
-printf("4. view all resources\n");
-printf("Your action: \n");
-scanf("%d",&action);
-return action;
-}
+int menu();
+void execute_action(int action);
+
 int main()
 {
+    char tittle[100] = "county library mamagement";
+    char status[100] = "welcome Patriki";
+    execute_action(menu());
     int action;
-    printf("COUNTY LIBRAY SYSTEM\n");
-    printf("Welcome Mr. Patriki\n");
-    action = menu();
-    printf("you selected action %d\n", action);
+    
+    printf("\n\n\n\t\t\t     **************\n\t\t\t%s\n \t\t\t      ****************", tittle, status);
+    menu();
     return 0;
 }
+int menu()
+{
+    int action;
+ printf("Select an action below\n");
+    printf("1. Add new Patron\n");
+    printf("2. view all patrons\n");
+    printf("3. Add new resources\n");
+    printf("4. View all resources\n");
+    printf("Your actions: \n");
+    scanf("%d", &action);
+    if(action < 1 || action > 4)
+       {
+           printf("invalid action");
+       }
+return action;
+}
+void execute_action(int action)
+{
+    printf("choice is: %d\n");
+    switch(action){
+        case 1:
+            printf("add a new patron\n");
+            break;
+        case 2:
+            printf("view all patrons\n");
+            break;
+        case 3:
+            printf("add new resources\m");
+            break; 
+        case 4:
+            printf("view all resources");
+            break; 
+        default:
+            printf("wrong entry");
+}
+}
+
